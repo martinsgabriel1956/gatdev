7d5283a27e5dba32c701d4cf344c500bb03f0edf
Gabriel <martingabriel1956@gmail.com>
2020-06-12 17:39:03 -0300

    Update artigo.php

--- artigo.php
+++ artigo.php
@@ -48,6 +48,26 @@ $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 //Se não tiver id, irá retornar para a listagem de artigos
 if ($id == 0) header('Location: /artigos.php');
 
+//Le o artigo do banco de dados
+
+$sql = "SELECT * FROM `artigos` WHERE id_artigo = '{$id}' AND status = 'ativo'";
+
+//Executa a Query
+$res = $conn->query($sql);
+
+//Obter os dados
+
+$art = $res->fetch_assoc();
+
+//DEBUG = print_r($art);
+
+//Formatar artigo
+$artigo = <<<HTML
+    <h2>{$art['titulo']}</h2>
+    <div>{$art['texto']}</div>
+
+HTML;
+
 //////////////////////////////////////////////////////
 // Seus códigos PHP para esta página terminam aqui! //
 // Teoricamente, não precisa alterar nada abaixo!   //
@@ -58,17 +78,11 @@ require ('_header.php');
 
 ?> 
 
-<h2>Proin vestibulum blandit</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur facilisis sapien vel ultrices molestie. Phasellus ut tellus feugiat, tincidunt orci in, lobortis urna. Morbi eu efficitur lectus. Nullam eleifend tellus quis consequat hendrerit. Vivamus nec hendrerit nibh. Nam auctor viverra lacus ut lobortis. In odio eros, semper a tellus eget, facilisis faucibus nulla.</p>
-<a href="http://luferat.net/">Site do Fessô</a>
-<p lang="en">Nullam scelerisque nisi est, a tincidunt nulla dictum vitae. Integer tincidunt felis lectus, eu sollicitudin mauris euismod vel. Aenean consectetur, arcu quis feugiat convallis, quam quam sollicitudin quam, in aliquet nulla ipsum et est. Donec in maximus magna, rutrum pretium augue. Donec quis pretium dui. Nulla tempor a velit eu finibus. Donec egestas ante vitae odio ultrices, vel egestas odio auctor. Nullam dictum lacus ut augue tempor ornare. Aenean vel nisl sit amet quam lobortis tempor. Ut id bibendum urna, at cursus justo. Quisque nisi risus, egestas in mi in, semper varius ipsum. Sed euismod bibendum pellentesque.</p>
-<img class="responsivo" src="https://picsum.photos/400/300" alt="Imagem aleatória">
-<p>Donec efficitur posuere commodo. Nulla in metus eu risus suscipit dignissim. Donec purus velit, varius nec placerat quis, gravida id velit. Vestibulum eget sodales metus. Sed rhoncus tellus augue, sit amet ultricies erat convallis vitae.</p>
-<p>Sed tincidunt ipsum justo, quis tempus enim tempus sed. Proin nisi nisi, commodo vel sollicitudin quis, efficitur ut purus. In hac habitasse platea dictumst. Curabitur hendrerit justo dolor, ac consequat nisi imperdiet ut. Nam in diam vel dolor viverra ultricies vel vel metus. Etiam semper luctus augue ac pellentesque. Nulla mi nisl, eleifend et aliquam eu, mattis vitae ligula.</p>
+<?php echo $artigo ?>
 
 <?php
 
 // Template de fechamento - rodapé
-require ('_footer.php');
+require '_footer.php';
 
 ?>
