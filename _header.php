1e6de53032f3f9806dd03c971c09f51f712c2f62
Gabriel <martingabriel1956@gmail.com>
2020-06-16 09:45:13 -0300

    Update _header.php

--- _header.php
+++ _header.php
@@ -52,7 +52,7 @@ if ( $js == "" ) {
                 <a <?php echo ($menu == 'noticias') ? 'class="active"' : null; ?> href="noticias.php"><i class="fas fa-fw fa-pen-nib"></i> Notícias</a>
                 <a <?php echo ($menu == 'contatos') ? 'class="active"' : null; ?> href="contatos.php"><i class="fas fa-fw fa-mail-bulk"></i> Contatos</a>
                 <a <?php echo ($menu == 'sobre') ? 'class="active"' : null; ?> href="sobre.php"><i class="fas fa-fw fa-info-circle"></i> Sobre</a>
-                <a <?php echo ($menu == 'procurar') ? 'class="active"' : null; ?> href="procurar.php" title="Procurar"><i class="fas fa-fw fa-search"></i><span>&nbsp;Procurar</span></a>
+               <!--<a <?php echo ($menu == 'procurar') ? 'class="active"' : null; ?> href="procurar.php" title="Procurar"><i class="fas fa-fw fa-search"></i><span>&nbsp;Procurar</span></a>-->
             </div>
             <a href="#menu" id="menu" title="Mostra / Oculta menu"><i class="fas fa-fw fa-bars"></i></a>
         </div>
