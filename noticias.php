533eece00a1c8ec1b43d0d207a924e4e3865deb1
Gabriel <martingabriel1956@gmail.com>
2020-06-15 23:45:03 -0300

    Update noticias.php

--- noticias.php
+++ noticias.php
@@ -13,21 +13,21 @@ require ('_config.php');
     $titulo --> Define o título "desta" página.
     Se = "", usa o título e o slogan do site.
 */
-$titulo = "Noticias";
+$titulo = "Notícias sobre gatos";
 
 /*
     $css --> Carrega o CSS adicional "desta" página.
     Se "", não carrega CSS adicional.
     Exemplo: /css/template.css
 */
-$css = "";
+$css = "css/noticias.css";
 
 /*
     $js --> Carrega o JavaScript adicional "desta" página.
     Se "", não usa JavaScript adicional.
     Exemplo: /js/template.js
 */
-$js = "";
+$js = "js/noticias.js";
 
 /*
     $menu --> Define o item ativo do menu "nesta" página.
@@ -35,12 +35,31 @@ $js = "";
     Valores possíveis: "", "artigos", "contatos", "sobre", "procurar", "noticias".
     Qualquer outro valor = "".
 */
-$menu = "";
+$menu = "noticias";
 
 /////////////////////////////////////////////////////
 // Seus códigos PHP para esta página começam aqui! //
 /////////////////////////////////////////////////////
 
+// Obtendo artigos aleatórios
+$sql = "SELECT id_artigo, imagem, titulo FROM `artigos` WHERE status = 'ativo' ORDER BY RAND() LIMIT 3;";
+$res = $conn->query($sql);
+
+// Loop de exibição
+$artigos = '';
+while ($art = $res->fetch_assoc()) :
+
+    $artigos .= <<<HTML
+<div class="art">
+<a href="/artigo.php?id={$art['id_artigo']}">
+    <img src="{$art['imagem']}" alt="{$art['titulo']}" title="{$art['titulo']}">    
+    {$art['titulo']}
+</a>
+</div>
+HTML;
+
+endwhile;
+$artigos .= '';
 
 
 //////////////////////////////////////////////////////
@@ -53,13 +72,17 @@ require ('_header.php');
 
 ?> 
 
-<h2>Proin vestibulum blandit</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur facilisis sapien vel ultrices molestie. Phasellus ut tellus feugiat, tincidunt orci in, lobortis urna. Morbi eu efficitur lectus. Nullam eleifend tellus quis consequat hendrerit. Vivamus nec hendrerit nibh. Nam auctor viverra lacus ut lobortis. In odio eros, semper a tellus eget, facilisis faucibus nulla.</p>
-<a href="http://luferat.net/">Site do Fessô</a>
-<p lang="en">Nullam scelerisque nisi est, a tincidunt nulla dictum vitae. Integer tincidunt felis lectus, eu sollicitudin mauris euismod vel. Aenean consectetur, arcu quis feugiat convallis, quam quam sollicitudin quam, in aliquet nulla ipsum et est. Donec in maximus magna, rutrum pretium augue. Donec quis pretium dui. Nulla tempor a velit eu finibus. Donec egestas ante vitae odio ultrices, vel egestas odio auctor. Nullam dictum lacus ut augue tempor ornare. Aenean vel nisl sit amet quam lobortis tempor. Ut id bibendum urna, at cursus justo. Quisque nisi risus, egestas in mi in, semper varius ipsum. Sed euismod bibendum pellentesque.</p>
-<img class="responsivo" src="https://picsum.photos/400/300" alt="Imagem aleatória">
-<p>Donec efficitur posuere commodo. Nulla in metus eu risus suscipit dignissim. Donec purus velit, varius nec placerat quis, gravida id velit. Vestibulum eget sodales metus. Sed rhoncus tellus augue, sit amet ultricies erat convallis vitae.</p>
-<p>Sed tincidunt ipsum justo, quis tempus enim tempus sed. Proin nisi nisi, commodo vel sollicitudin quis, efficitur ut purus. In hac habitasse platea dictumst. Curabitur hendrerit justo dolor, ac consequat nisi imperdiet ut. Nam in diam vel dolor viverra ultricies vel vel metus. Etiam semper luctus augue ac pellentesque. Nulla mi nisl, eleifend et aliquam eu, mattis vitae ligula.</p>
+<div class="row">
+    <div class="col1">
+        <h2>Notícias da Gatolândia</h2>
+        <p>Últimas notícias do Brasil e do mundo sobre felinos e pets em geral.</p>
+        <div id="news"></div>
+    </div>
+    <aside class="col2">
+        <h3>Artigos</h3>
+        <?php echo $artigos ?>
+    </aside>
+</div>
 
 <?php
 
